88c9f0e013ea7601a7447b012411d24fc8206747
John Duimovich <jduimovich@gmail.com>
2023-03-02 14:17:47 -0500

    auto-install clowder

--- hack/install_clowder.sh
+++ hack/install_clowder.sh
@@ -2,13 +2,9 @@
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/..
 # check out your own copy first and it will use it, instead of cloning a new one
 CLOWDER=$ROOT/../clowder 
-if [ -d $CLOWDER ]; then
-  (cd $CLOWDER; ./build/kube_setup.sh)
-else
+if [ ! -d $CLOWDER ]; then 
   echo "No clowder found in $CLOWDER"
-  echo "git clone https://github.com/RedHatInsights/clowder.git" 
-  echo "into the parent directory ../clowder"
-  echo "and re-reun this script"  
-  exit 1
-fi
-  
+  echo "Cloning it into the parent directory for installation." 
+  git clone https://github.com/RedHatInsights/clowder.git $CLOWDER 
+fi 
+(cd $CLOWDER; ./build/kube_setup.sh)
